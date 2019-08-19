903dfb3369599712eb1391bb5591e4c733e037b0
ArshiA <letmemakenewone@gmail.com>
2019-08-19 17:20:07 +0430

    class Game

--- src/LeopardGame.java
+++
@@ -1,5 +0,0 @@
-public class LeopardGame extends Menu {
-    public LeopardGame(Menu parentMenu, String name) {
-        super(parentMenu, name);
-    }
-}
--- src/consoleView.java
+++
@@ -1,4 +0,0 @@
-public class consoleView {
-    public static void main(String[] args) {
-    }
-}
